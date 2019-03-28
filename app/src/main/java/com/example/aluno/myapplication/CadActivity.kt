9a67038e4b1f70bde2d4d737bd961d2736f7e124
AnaBertolucci <anabertolucci14@gmail.com>
2019-03-27 23:39:50 -0300

    Tela cad

---
+++ app/src/main/java/com/example/aluno/myapplication/CadActivity.kt
@@ -0,0 +1,24 @@
+package com.example.aluno.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+import org.jetbrains.anko.toast
+
+
+
+class CadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cad)
+
+
+        }
+
+        }
+
+
